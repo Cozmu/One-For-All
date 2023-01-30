414d86a704a1cd628ff381fc6e726520444b2fd3
Cozmu <www.jorgereis@gmail.com>
2023-01-30 19:47:35 -0300

    feat: add desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,13 @@
 -- Active: 1674757442069@@127.0.0.1@3307@SpotifyClone
 SELECT 
     son.songs_name AS nome,
-    COUNT(`use`.plans_id) AS reproducoes
-FROM `SpotifyClone`.songs AS son
-INNER JOIN `SpotifyClone`.playback_history AS play
-ON son.id = play.name_songs_played_id
+    COUNT(*) AS reproducoes
+FROM `SpotifyClone`.playback_history AS play 
+INNER JOIN `SpotifyClone`.songs AS son
+ON play.name_songs_played_id = son.id 
 INNER JOIN `SpotifyClone`.users AS `use`
-ON `use`.`id` = play.user_id
-WHERE `use`.plans_id = 1 OR `use`.plans_id = 4
-GROUP BY nome
-ORDER BY nome;
+ON play.user_id = `use`.`id` 
+WHERE `use`.plans_id = 1 OR `use`.plans_id = 3
+GROUP BY son.songs_name
+ORDER BY son.songs_name;
+
